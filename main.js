0a93dc57fb5c4872351884d67c61a349049703b2
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-03-28 10:38:33 +0200

    some changes

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
 var playerNow = document.querySelector('.js-playerNow'),
-		role = document.querySelector('.js-doRole'),
-		repeat = document.querySelector('.js-repeat');		
+    role = document.querySelector('.js-doRole'),
+    repeat = document.querySelector('.js-repeat'),
+    inputAdd = document.querySelector('.js-inputAdd'),
+    addPlayerBtn = document.querySelector('.js-addPlayer');		
 		
 const players = ['Ваня', 'Валера', 'Влад', 'Бодя'];
+// var players = [];
 
-function randPlayer() {
+window.onload = function(){
+    mainFunc(); // display player name for start screen
+}
+
+role.addEventListener('click', mainFunc);
+addPlayerBtn.addEventListener('click', addNewPlayer);
+
+function mainFunc() {
     var rand = Math.floor(Math.random() * players.length);
-
     var res = players[rand];
         
-//         if ( res == playerNow.innerHTML ) {
-//             console.log('1');
-//             randPlayer()
-//         } else {
-//             console.log(playerNow.innerHTML)
-//             playerNow.innerHTML = res;
-//         }
-        
-        if ( repeat.checked ) {
-//             randPlayer();
-                playerNow.innerHTML = res;
-        console.log('Repeat: ' + res);
-    } else {
+    // if repeat player is true
+    if ( repeat.checked ) {
         playerNow.innerHTML = res;
-                    console.log('No repeat: ' + playerNow.innerHTML);
+        console.log('Repeat: ' + res);
+    }
+    // if repeat player is false
+    else {
+        
+        if ( playerNow.innerHTML == res ) {
+            mainFunc(); // get new player name if this name is equal playerNow
+            console.log('No repeat: ' + playerNow.innerHTML);
+        }
+        else {
+            playerNow.innerHTML = res; // get player name if this name is not equal playerNow
+            console.log('No repeat: ' + playerNow.innerHTML);
+        }
+
+        
     }
 
 }
 
-role.addEventListener('click', randPlayer);
+window.onload = function addNewPlayer() {
+    // var inputVal = inputAdd.value;
+    var playersItem = document.querySelector('.players-item');
 
-window.onload = function(){
-    randPlayer();
+    playersItem.innerHTML = inputAdd.value;
 }
