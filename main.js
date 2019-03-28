a40ee4d029eb846177e34b46d86640d47ecb14c0
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-03-28 16:58:41 +0200

    Add new functionality

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
-var playerNow = document.querySelector('.js-playerNow'),
+window.onload = function(){
+    var playerNow = document.querySelector('.js-playerNow'),
     role = document.querySelector('.js-doRole'),
     repeat = document.querySelector('.js-repeat'),
     inputAdd = document.querySelector('.js-inputAdd'),
-    addPlayerBtn = document.querySelector('.js-addPlayer');		
+    addPlayerBtn = document.querySelector('.js-addPlayer'),
+    playersList = document.querySelector('.players-list');		
 		
-const players = ['Ваня', 'Валера', 'Влад', 'Бодя'];
-// var players = [];
+    // const players = ['Ваня', 'Валера', 'Влад', 'Бодя'];
+    var players = [];
 
-window.onload = function(){
     mainFunc(); // display player name for start screen
-}
 
-role.addEventListener('click', mainFunc);
-addPlayerBtn.addEventListener('click', addNewPlayer);
-
-function mainFunc() {
-    var rand = Math.floor(Math.random() * players.length);
-    var res = players[rand];
-        
-    // if repeat player is true
-    if ( repeat.checked ) {
-        playerNow.innerHTML = res;
-        console.log('Repeat: ' + res);
-    }
-    // if repeat player is false
-    else {
-        
-        if ( playerNow.innerHTML == res ) {
-            mainFunc(); // get new player name if this name is equal playerNow
-            console.log('No repeat: ' + playerNow.innerHTML);
+    role.addEventListener('click', mainFunc);
+    addPlayerBtn.addEventListener('click', addNewPlayer);
+    // add to list by clicking on enter
+    inputAdd.addEventListener('keyup', function(event){
+        if(event.keyCode == 13){
+            addNewPlayer();
         }
+    });
+
+    function mainFunc() {
+        var rand = Math.floor(Math.random() * players.length);
+        var res = players[rand];
+            
+        // if repeat player is true
+        if ( repeat.checked ) {
+            playerNow.innerHTML = res;
+            console.log('Repeat: ' + res);
+        }
+        // if repeat player is false
         else {
-            playerNow.innerHTML = res; // get player name if this name is not equal playerNow
-            console.log('No repeat: ' + playerNow.innerHTML);
+            
+            if ( playerNow.innerHTML == res ) {
+                mainFunc(); // get new player name if this name is equal playerNow
+                console.log('No repeat: ' + playerNow.innerHTML);
+            }
+            else {
+                playerNow.innerHTML = res; // get player name if this name is not equal playerNow
+                console.log('No repeat: ' + playerNow.innerHTML);
+            }
+            
         }
-
-        
     }
 
-}
+    function addNewPlayer() {
+        players.push(inputAdd.value);
+        inputAdd.value = '';
 
-window.onload = function addNewPlayer() {
-    // var inputVal = inputAdd.value;
-    var playersItem = document.querySelector('.players-item');
+        console.log(players);
 
-    playersItem.innerHTML = inputAdd.value;
+        renderNewPlayer();
+    }
+
+    function renderNewPlayer() {
+        for (var i = 0; i < players.length; i++) {
+            playersList.insertAdjacentHTML('afterBegin', `<li>${players[i]}</li>`);
+        }
+    }
+
 }
